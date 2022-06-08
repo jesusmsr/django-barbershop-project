565c5da74c9276e4778e894b522759d176bbc8ac
sanchezj <jesus.sanchez@creamteam.de>
2022-06-08 14:30:54 +0200

    Fix typo in urls

--- booking_app/api/urls.py
+++ booking_app/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from booking_app.api.views import BookingListAV, BookingCreateAV
 
-urlpatterns = {
+urlpatterns = [
     path('list/', BookingListAV.as_view(), name='booking-list'),
     path('booking-create/', BookingCreateAV.as_view(), name='booking-create'),
-}
+]
