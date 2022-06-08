1ad837253231e226bcdc7a8203bf0c1eefcd1973
sanchezj <jesus.sanchez@creamteam.de>
2022-06-08 14:19:53 +0200

    Add endpoint to create bookings

--- booking_app/api/urls.py
+++ booking_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from booking_app.api.views import BookingAV
+from booking_app.api.views import BookingListAV, BookingCreateAV
 
 urlpatterns = {
-    path('list/', BookingAV.as_view(), name='booking-list'),
+    path('list/', BookingListAV.as_view(), name='booking-list'),
+    path('booking-create/', BookingCreateAV.as_view(), name='booking-create'),
 }
--- booking_app/api/views.py
+++ booking_app/api/views.py
@@ -3,13 +3,26 @@ from booking_app.models import Booking
 from rest_framework.response import Response
 from booking_app.api.serializers import BookingSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 
-
-class BookingAV(APIView):
+class BookingListAV(APIView):
     permission_classes = [IsAuthenticated]
     
     def get(self, request):
         bookings = Booking.objects.all()
         serializer = BookingSerializer(bookings, many=True, context={'request': request})
         return Response(serializer.data)
+    
+class BookingCreateAV(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        serializer = BookingSerializer(data=request.data)
+        user = self.request.user
+        
+        if serializer.is_valid():
+            serializer.save(user=user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
