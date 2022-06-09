e7562970e6a4934a1ee4308b9c14653c60559887
jesusmsr <jsr20xx@gmail.com>
2022-06-09 09:46:57 +0200

    Fix issu with login endpoint not picking up json request

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -33,24 +33,24 @@ def register_view(request):
 def login_view(request):
     data = {}
     if request.method == 'POST':
-        email = request.post.get('email')
-        password = request.post.get('password')
+        email = request.data.get('email')
+        password = request.data.get('password')
         
-    account = auth.authenticate(email=email, password=password)
-    if account is not None:
-        data['response'] = 'Login successfull'
-        data['username'] = account.username
-        data['email'] = account.email
-        data['first_name'] = account.first_name
-        data['last_number'] = account.last_name
-        data['phone_number'] = account.phone_number
-        refresh = RefreshToken.for_user(account)
-        data['token'] = {
-            'refresh': str(refresh),
-            'access': str(refresh.access_token)
-        }
-        
-        return Response(data)
-    else:
-        data['error'] = 'bad credentials'
-        return Response(data, status=status.HTTP_INTERNAL_SERVER_ERROR)
+        account = auth.authenticate(email=email, password=password)
+        if account is not None:
+            data['response'] = 'Login successfull'
+            data['username'] = account.username
+            data['email'] = account.email
+            data['first_name'] = account.first_name
+            data['last_number'] = account.last_name
+            data['phone_number'] = account.phone_number
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token)
+            }
+            
+            return Response(data)
+        else:
+            data['error'] = 'bad credentials'
+            return Response(data, status=status.HTTP_INTERNAL_SERVER_ERROR)
